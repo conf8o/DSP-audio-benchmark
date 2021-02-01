4f5eb8ccd3918248e26d2597ce414fa232c4e0d2
conf8o <8confeito8@gmail.com>
2021-02-01 16:12:45 +0900

    Sin wave - playground of Rust

--- playground/rust/src/main.rs
+++ playground/rust/src/main.rs
@@ -1,10 +1,13 @@
-use rustfft::{FftPlanner, num_complex::Complex};
+use ndarray::Array1;
 
 fn main() {
-    let mut planner = FftPlanner::new();
-    let fft = planner.plan_fft_forward(1024);
+    println!("{}", sin_wave(2., 16000, 100));
+}
 
-    let mut buffer = vec![Complex{ re: 0.0f32, im: 0.0f32 }; 1024];
-    fft.process(&mut buffer);
+fn sin_wave(k: f64, rate: u32, ms: u32) -> Array1<f64> {
+    let end = (ms as f64) / 1000.;
+    let n = (rate * ms / 1000) as usize;
+    let w = 2. * std::f64::consts::PI * k;
 
+    (Array1::linspace(0., end, n) * w).mapv(f64::sin)
 }
