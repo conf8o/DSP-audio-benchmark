c79c615ba2c500ce2b69b6a34708db355a6365db
conf8o <8confeito8@gmail.com>
2021-02-02 19:37:25 +0900

    Sin wave - playground of Rust

--- playground/rust/src/main.rs
+++ playground/rust/src/main.rs
@@ -1,13 +1,21 @@
 use ndarray::Array1;
+use plotters::prelude::*;
 
-fn main() {
-    println!("{}", sin_wave(2., 16000, 100));
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let root = BitMapBackend::new("output/0.png", (640, 480)).into_drawing_area();
+    root.fill(&WHITE)?;
+    
+    let mut chart = ChartBuilder::on(&root)
+        .build_cartesian_2d(0f32..1f32, -1f32..1f32)?;
+
+    chart.configure_mesh().draw()?;
+
+    let times = sampling_axis(0., 1., 16000.);
+    let w = 2. * std::f32::consts::PI * 10.;
+    chart.draw_series(LineSeries::new(times.iter().map(|x| (*x, (w * x).sin())), &RED))?;
+    Ok(())
 }
 
-fn sin_wave(k: f64, rate: u32, ms: u32) -> Array1<f64> {
-    let end = (ms as f64) / 1000.;
-    let n = (rate * ms / 1000) as usize;
-    let w = 2. * std::f64::consts::PI * k;
-
-    (Array1::linspace(0., end, n) * w).mapv(f64::sin)
+fn sampling_axis(start: f32, end: f32, rate: f32) -> Array1<f32> {
+    Array1::range(start, end, 1. / rate)
 }
