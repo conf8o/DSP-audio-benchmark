dce9ac0747f64fec4a96fa0305c7286c38a2a6fe
conf8o <8confeito8@gmail.com>
2021-02-16 17:02:34 +0900

    Improve appearance of STFT

--- playground/rust/src/main.rs
+++ playground/rust/src/main.rs
@@ -21,7 +21,7 @@ fn fft(fft_func: &std::sync::Arc<dyn Fft<f32>>, signal: Array1<f32>) -> Array1<f
 
 fn stft(data: Array1<f32>, frame_len: usize, hop_len: usize) -> Array2::<f32> {
     // STFTの結果格納用
-    let mut frames = Array2::<f32>::zeros((data.len() / hop_len, frame_len));
+    let mut stft_frames = Array2::<f32>::zeros((data.len() / hop_len, frame_len));
 
     // ハミング窓
     let window = hamming(frame_len);
@@ -29,28 +29,28 @@ fn stft(data: Array1<f32>, frame_len: usize, hop_len: usize) -> Array2::<f32> {
     let fft_func = planner.plan_fft_forward(frame_len);
 
     // 行ごとにアサインしていく
-    for (i, mut row) in frames.axis_iter_mut(Axis(0)).enumerate() {
-        let ffted = {
+    for (i, mut ffted_frame) in stft_frames.axis_iter_mut(Axis(0)).enumerate() {
+        let windowed_frame = {
             let end = cmp::min(i*hop_len+frame_len, data.len());
             let data_slice = &data.slice(s![i*hop_len..end]);
             
-            let mut fft_buf = Array1::<f32>::zeros(frame_len);
-            fft_buf
+            let mut frame = Array1::<f32>::zeros(frame_len);
+            frame
             .slice_mut(s![..data_slice.len()])
             .assign(data_slice);
 
             // 窓関数適用
-            fft_buf *= &window;
-
-            // FFT
-            fft_buf = fft(&fft_func, fft_buf);
-            fft_buf
+            frame *= &window;
+            frame
         };
 
-        row.assign(&ffted);
+        // FFT
+        let ffted = fft(&fft_func, windowed_frame);
+
+        ffted_frame.assign(&ffted);
     }
     
-    frames
+    stft_frames
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
