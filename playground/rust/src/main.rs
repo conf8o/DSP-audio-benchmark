dab853885080eafcd363766cf686a6efc627bcbf
conf8o <8confeito8@gmail.com>
2021-07-06 11:58:57 +0900

    STFTの構造体

--- playground/rust/src/main.rs
+++ playground/rust/src/main.rs
@@ -19,43 +19,57 @@ fn fft(fft_func: &std::sync::Arc<dyn Fft<f32>>, signal: Array1<f32>) -> Array1<f
     .collect::<Array1<f32>>()
 }
 
-fn stft(data: Array1<f32>, frame_len: usize, hop_len: usize) -> Array2::<f32> {
-    // STFTの結果格納用
-    let mut stft_frames = Array2::<f32>::zeros((data.len() / hop_len, frame_len));
+struct Stft {
+    fft_func: std::sync::Arc<dyn Fft<f32>>,
+    window: Array1<f32>,
+    hop_len: usize
+}
 
-    // ハミング窓
-    let window = hamming(frame_len);
-    let mut planner = FftPlanner::new();
-    let fft_func = planner.plan_fft_forward(frame_len);
+impl Stft {
+    fn new(frame_len: usize, hop_len: usize) -> Stft {
+        let mut planner = FftPlanner::new();
+        let fft_func = planner.plan_fft_forward(frame_len);
 
-    // 行ごとにアサインしていく
-    for (i, mut ffted_frame) in stft_frames.axis_iter_mut(Axis(0)).enumerate() {
-        let windowed_frame = {
-            let end = cmp::min(i*hop_len+frame_len, data.len());
-            let data_slice = &data.slice(s![i*hop_len..end]);
-            
-            let mut frame = Array1::<f32>::zeros(frame_len);
-            frame
-            .slice_mut(s![..data_slice.len()])
-            .assign(data_slice);
-
-            // 窓関数適用
-            frame *= &window;
-            frame
-        };
-
-        // FFT
-        let ffted = fft(&fft_func, windowed_frame);
-
-        ffted_frame.assign(&ffted);
+        Stft{ fft_func: fft_func, window: hamming(frame_len), hop_len: hop_len }
     }
+
+    fn process(&self, data: Array1<f32>) -> Array2::<f32> {
+        let frame_len = self.window.len();
+
+        // STFTの結果格納用
+        let mut stft_frames = Array2::<f32>::zeros((data.len() / self.hop_len, frame_len));
+
+        // 行ごとに代入していく
+        for (i, mut ffting_frame) in stft_frames.axis_iter_mut(Axis(0)).enumerate() {
+            let windowed_frame = {
+                let mut frame = Array1::<f32>::zeros(frame_len);
+
+                let end = cmp::min(i * self.hop_len + frame_len, data.len());
+                let data_slice = &data.slice(s![(i*self.hop_len)..end]);
+                
+                // 窓関数適用
+                frame
+                .slice_mut(s![..data_slice.len()])
+                .assign(&(data_slice * &self.window));
+
+                frame
+            };
+
+            // FFT
+            let ffted = fft(&self.fft_func, windowed_frame);
+
+            ffting_frame.assign(&ffted);
+        }
     
-    stft_frames
+        stft_frames
+    }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let a = array![1.0, 2.0, 3.0];
-    stft(a, 3, 2);
+    let stft = Stft::new(3, 2);
 
+    let a = array![1.0, 2.0, 3.0];
+
+    println!("{}", stft.process(a));
     Ok(())
 }
